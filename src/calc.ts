0e674ac33fe4ba3350a1e656678ac69132f9e2b6
Guilherme Carneiro <gcomartins@gmail.com>
2022-02-05 11:15:13 -0300

    Fatoração de 1 a 20

--- src/calc.ts
+++ src/calc.ts
@@ -1,3 +1,71 @@
-export function conversor(numero: number): string {
-    
+export function converter(numero: number): string {
+  if (numero < 0) {
+    throw new Error("Número inconversível!");
+  }
+  if (numero === 0) {
+    return "Zero";
+  }
+  if (numero === 1) {
+    return "I";
+  }
+  if (numero === 2) {
+    return "II";
+  }
+  if (numero === 3) {
+    return "III";
+  }
+  if (numero === 4) {
+    return "IV";
+  }
+  if (numero === 5) {
+    return "V";
+  }
+  if (numero === 6) {
+    return "VI";
+  }
+  if (numero === 7) {
+    return "VII";
+  }
+  if (numero === 8) {
+    return "VIII";
+  }
+  if (numero === 9) {
+    return "IV";
+  }
+  if (numero === 10) {
+    return "X";
+  }
+  if (numero === 11) {
+    return "XI";
+  }
+  if (numero === 12) {
+    return "XII";
+  }
+  if (numero === 13) {
+    return "XIII";
+  }
+  if (numero === 14) {
+    return "XIV";
+  }
+  if (numero === 15) {
+    return "XV";
+  }
+  if (numero === 16) {
+    return "XVI";
+  }
+  if (numero === 17) {
+    return "XVII";
+  }
+  if (numero === 18) {
+    return "XVIII";
+  }
+  if (numero === 19) {
+    return "XIX";
+  }
+  if (numero === 20) {
+    return "XX";
+  }
+  return "Número desconhecido!";
 }
+
+
--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -1,6 +1,126 @@
-import { conversor } from "../src/calc";
-describe("Testes do conversor romano", () => {
-  it("Tem que retornar I quando conversor(1)", () => {
-    expect(conversor(1)).toBe("I");
+import { converter } from "../src/calc";
+// describe("Conversor Romano", () => {
+//   it("Tem que lançar exceção quando for passado algum número menor que 0", () => {
+//     expect(converter(-1)).toThrow();
+//   });
+// });
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar I quando converter(1)", () => {
+    expect(converter(1)).toBe("I");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar II quando converter(2)", () => {
+    expect(converter(2)).toBe("II");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar III quando converter(3)", () => {
+    expect(converter(3)).toBe("III");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar IV quando converter(4)", () => {
+    expect(converter(4)).toBe("IV");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar V quando converter(5)", () => {
+    expect(converter(5)).toBe("V");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar VI quando converter(6)", () => {
+    expect(converter(6)).toBe("VI");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar VII quando converter(7)", () => {
+    expect(converter(7)).toBe("VII");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar VIII quando converter(8)", () => {
+    expect(converter(8)).toBe("VIII");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar IV quando converter(9)", () => {
+    expect(converter(9)).toBe("IV");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar X quando converter(10)", () => {
+    expect(converter(10)).toBe("X");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XI quando converter(11)", () => {
+    expect(converter(11)).toBe("XI");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XII quando converter(12)", () => {
+    expect(converter(12)).toBe("XII");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XIII quando converter(13)", () => {
+    expect(converter(13)).toBe("XIII");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XIV quando converter(14)", () => {
+    expect(converter(14)).toBe("XIV");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XV quando converter(15)", () => {
+    expect(converter(15)).toBe("XV");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XVI quando converter(16)", () => {
+    expect(converter(16)).toBe("XVI");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XVII quando converter(17)", () => {
+    expect(converter(17)).toBe("XVII");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XVIII quando converter(18)", () => {
+    expect(converter(18)).toBe("XVIII");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XIX quando converter(19)", () => {
+    expect(converter(19)).toBe("XIX");
+  });
+});
+
+describe("Conversor Romano", () => {
+  it("Tem que retornar XX quando converter(20)", () => {
+    expect(converter(20)).toBe("XX");
   });
 });
