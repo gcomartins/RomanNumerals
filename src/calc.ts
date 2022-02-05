acc91bbbd77fbb60023e47cf249093ac30cef540
Guilherme Carneiro <gcomartins@gmail.com>
2022-02-05 13:58:41 -0300

    Refatoração do código

--- src/calc.ts
+++ src/calc.ts
@@ -6,68 +6,27 @@ export function converter(numero: number): string {
     return "Os romanos não tinham um sinal para representar o zero porque simplesmente não precisavam representar o nada. Essa necessidade só surgiu quando se criou um sistema numérico posicional, ou seja, um sistema no qual a posição dos algarismos dá a eles valores diferentes.";
   }
   let resultado: string = "";
-  while (numero >= 1000) {
-    resultado += "M";
-    numero -= 1000;
-  }
-  while (numero >= 900) {
-    resultado += "CM";
-    numero -= 900;
-  }
-  while (numero >= 500) {
-    resultado += "D";
-    numero -= 500;
-  }
-  while (numero >= 400) {
-    resultado += "CD";
-    numero -= 400;
-  }
-  while (numero >= 100) {
-    resultado += "C";
-    numero -= 100;
-  }
-  while (numero >= 90) {
-    resultado += "XC";
-    numero -= 90;
-  }
-  while (numero >= 50) {
-    resultado += "L";
-    numero -= 50;
-  }
-  while (numero >= 40) {
-    resultado += "XL";
-    numero -= 40;
-  }
-  while (numero >= 10) {
-    resultado += "X";
-    numero -= 10;
-  }
-  if (numero === 1) {
-    resultado += "I";
-  }
-  if (numero === 2) {
-    resultado += "II";
-  }
-  if (numero === 3) {
-    resultado += "III";
-  }
-  if (numero === 4) {
-    resultado += "IV";
-  }
-  if (numero === 5) {
-    resultado += "V";
-  }
-  if (numero === 6) {
-    resultado += "VI";
-  }
-  if (numero === 7) {
-    resultado += "VII";
-  }
-  if (numero === 8) {
-    resultado += "VIII";
-  }
-  if (numero === 9) {
-    resultado += "IX";
+  const simbolos: string[] = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ];
+  const numeros: number[] = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  for (let index = 0; index < 14; index++) {
+    while (numero >= numeros[index]) {
+      resultado += simbolos[index];
+      numero-= numeros[index];
+    }
   }
   return resultado;
 }
